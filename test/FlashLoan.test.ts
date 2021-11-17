03ac6ffc9173b0290b5a08d5f9c0de91f32e5e6b
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-11-17 13:28:58 +0100

    renamed signers, added connect method

--- test/FlashLoan.test.ts
+++ test/FlashLoan.test.ts
@@ -16,114 +16,103 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("Flash Loan Contract", () => {
   let flashLoan: FlashLoan;
-  let depositSecondConnected: FlashLoan;
+  // let flashLoanBorrower: FlashLoan;
 
-  let myToken: MyToken, myUserToken: MyToken;
+  let myToken: MyToken;
 
-  let owner: SignerWithAddress, second: SignerWithAddress;
+  let diablo: SignerWithAddress, alwin: SignerWithAddress;
 
   let borrower: Borrower;
   let reentrance: Reentrance;
   let thief: Thief;
 
   beforeEach(async () => {
-    [owner, second] = await ethers.getSigners();
-    flashLoan = await new FlashLoan__factory(owner).deploy(100);
-    myToken = await new MyToken__factory(owner).deploy("MyToken", "MTK");
-
-    depositSecondConnected = flashLoan.connect(second);
-    myUserToken = myToken.connect(second);
+    [diablo, alwin] = await ethers.getSigners();
+    flashLoan = await new FlashLoan__factory(diablo).deploy(100);
+    myToken = await new MyToken__factory(diablo).deploy("MyToken", "MTK");
   });
 
   describe("Borrow function", () => {
     it("borrower returns correct amount of tokens", async () => {
-      borrower = await new Borrower__factory(second).deploy();
-      await myToken.mint(owner.address, 100000);
+      borrower = await new Borrower__factory(alwin).deploy();
+      await myToken.mint(diablo.address, 100000);
       await myToken.approve(flashLoan.address, 100000);
 
       expect(
-        (await myToken.allowance(owner.address, flashLoan.address)).toString()
+        (await myToken.allowance(diablo.address, flashLoan.address)).toString()
       ).to.be.equal("100000");
 
-      await myToken.mint(second.address, 1000);
+      await myToken.mint(alwin.address, 1000);
 
-      await myUserToken.approve(borrower.address, 2000);
-      await depositSecondConnected.borrowTokens(
-        borrower.address,
-        myToken.address,
-        1000
-      );
+      await myToken.connect(alwin).approve(borrower.address, 2000);
+      await flashLoan
+        .connect(alwin)
+        .borrowTokens(borrower.address, myToken.address, 1000);
 
-      expect((await myToken.balanceOf(owner.address)).toString()).to.be.equal(
+      expect((await myToken.balanceOf(diablo.address)).toString()).to.be.equal(
         "100010"
       );
-      expect((await myToken.balanceOf(second.address)).toString()).to.be.equal(
+      expect((await myToken.balanceOf(alwin.address)).toString()).to.be.equal(
         "990"
       );
     });
 
     it("borrower returns too less tokens - revert", async () => {
-      borrower = await new Borrower__factory(second).deploy();
-      await myToken.mint(owner.address, 100000);
+      borrower = await new Borrower__factory(alwin).deploy();
+      await myToken.mint(diablo.address, 100000);
       await myToken.approve(flashLoan.address, 100000);
 
       expect(
-        (await myToken.allowance(owner.address, flashLoan.address)).toString()
+        (await myToken.allowance(diablo.address, flashLoan.address)).toString()
       ).to.be.equal("100000");
 
-      await myUserToken.approve(borrower.address, 2000);
+      await myToken.connect(alwin).approve(borrower.address, 2000);
 
       await expect(
-        depositSecondConnected.borrowTokens(
-          borrower.address,
-          myToken.address,
-          1000
-        )
+        flashLoan
+          .connect(alwin)
+          .borrowTokens(borrower.address, myToken.address, 1000)
       ).to.be.reverted;
 
-      expect((await myToken.balanceOf(owner.address)).toString()).to.be.equal(
+      expect((await myToken.balanceOf(diablo.address)).toString()).to.be.equal(
         "100000"
       );
-      expect((await myToken.balanceOf(second.address)).toString()).to.be.equal(
+      expect((await myToken.balanceOf(alwin.address)).toString()).to.be.equal(
         "0"
       );
     });
     it("reentrance flash loan - revert", async () => {
-      reentrance = await new Reentrance__factory(second).deploy(
+      reentrance = await new Reentrance__factory(alwin).deploy(
         flashLoan.address
       );
 
-      await myToken.mint(owner.address, 100000);
+      await myToken.mint(diablo.address, 100000);
       await myToken.approve(flashLoan.address, 100000);
 
-      await myToken.mint(second.address, 10000);
-      await myUserToken.approve(reentrance.address, 20000);
+      await myToken.mint(alwin.address, 10000);
+      await myToken.connect(alwin).approve(reentrance.address, 20000);
 
       await expect(
-        depositSecondConnected.borrowTokens(
-          reentrance.address,
-          myToken.address,
-          1000
-        )
+        flashLoan
+          .connect(alwin)
+          .borrowTokens(reentrance.address, myToken.address, 1000)
       ).to.be.reverted;
     });
 
     it("thief borrower doesn't return tokens - revert", async () => {
-      thief = await new Thief__factory(second).deploy();
+      thief = await new Thief__factory(alwin).deploy();
 
-      await myToken.mint(owner.address, 100000);
+      await myToken.mint(diablo.address, 100000);
       await myToken.approve(flashLoan.address, 100000);
 
-      await myToken.mint(second.address, 1000);
-      await myUserToken.approve(thief.address, 2000);
+      await myToken.mint(alwin.address, 1000);
+      await myToken.connect(alwin).approve(thief.address, 2000);
 
       await expect(
-        depositSecondConnected.borrowTokens(
-          thief.address,
-          myToken.address,
-          1000
-        )
-      ).to.be.revertedWith("incorrect returned value");
+        flashLoan
+          .connect(alwin)
+          .borrowTokens(thief.address, myToken.address, 1000)
+      ).to.be.revertedWith("Incorrect returned value");
     });
   });
 });

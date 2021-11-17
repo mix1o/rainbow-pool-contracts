2cc73f197ba5ae6f50b011da6718af6d69326688
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-11-17 12:16:26 +0100

    hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/types";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.1",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;

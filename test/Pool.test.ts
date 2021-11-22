9237114310e4b0e2f8ac496ddbc26db36234bf73
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-11-22 13:37:27 +0100

    Created tests for Pool contract, checking function withdraw and methods of cheat

--- test/Pool.test.ts
+++ test/Pool.test.ts
@@ -8,12 +8,12 @@ import {
   Borrower,
   Borrower__factory,
   LpToken,
+  LpToken__factory,
   Pool,
   Pool__factory,
-  LpToken__factory,
-  Borrowers__factory,
 } from "../typechain-types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { parseUnits } from "@ethersproject/units";
 
 describe("Pool Contract", () => {
   let flashLoan: FlashLoan;
@@ -25,16 +25,22 @@ describe("Pool Contract", () => {
   let diablo: SignerWithAddress,
     alwin: SignerWithAddress,
     poolOwner: SignerWithAddress,
-    tokenOwner: SignerWithAddress;
+    tokenOwner: SignerWithAddress,
+    cheater: SignerWithAddress;
 
   let borrower: Borrower;
 
   beforeEach(async () => {
-    [diablo, alwin, poolOwner, tokenOwner] = await ethers.getSigners();
+    [diablo, alwin, poolOwner, tokenOwner, cheater] = await ethers.getSigners();
 
     pool = await new Pool__factory(poolOwner).deploy();
-    lpToken = await new LpToken__factory(poolOwner).deploy("LpToken", "LPT");
     myToken = await new MyToken__factory(tokenOwner).deploy("MyToken", "MTK");
+    lpToken = await new LpToken__factory(poolOwner).deploy(
+      "LpToken",
+      "LPT",
+      pool.address,
+      myToken.address
+    );
 
     flashLoan = await new FlashLoan__factory(poolOwner).deploy(
       100,
@@ -42,47 +48,84 @@ describe("Pool Contract", () => {
     );
 
     borrower = await new Borrower__factory(alwin).deploy();
+
+    await myToken.mint(diablo.address, parseUnits("30"));
+    await myToken.mint(alwin.address, parseUnits("20"));
+
+    await myToken.connect(diablo).approve(pool.address, parseUnits("30"));
+    await myToken.connect(alwin).approve(borrower.address, parseUnits("20"));
+
+    await lpToken.grantPoolRole();
+
+    await pool.setLpToken(lpToken.address);
+    await pool.setToken(myToken.address);
+    await pool.setInitialAllowance(flashLoan.address);
+
+    await pool.connect(diablo).deposit(parseUnits("30"));
+
+    expect((await myToken.balanceOf(diablo.address)).toString()).to.be.equal(
+      "0"
+    );
+
+    await flashLoan
+      .connect(alwin)
+      .borrowTokens(borrower.address, myToken.address, parseUnits("10"));
   });
 
-  describe("pool", () => {
-    it.only("deposit", async () => {
-      await myToken.mint(diablo.address, 10000);
-      await myToken.connect(diablo).approve(pool.address, 10000);
+  it("user calls function collectRewards twice - should get only one reward", async () => {
+    await lpToken.connect(diablo).collectRewards();
 
-      await lpToken.grantPoolRole(pool.address);
-      await pool.setLpToken(lpToken.address);
-      await pool.setToken(myToken.address);
+    expect((await myToken.balanceOf(diablo.address)).toString()).to.be.equal(
+      "99999999999999990"
+    );
 
-      await pool.connect(diablo).deposit(10000);
+    await lpToken.connect(diablo).collectRewards();
 
-      console.log("pool t", (await myToken.balanceOf(pool.address)).toString());
+    expect((await myToken.balanceOf(diablo.address)).toString()).to.be.equal(
+      "99999999999999990"
+    );
+  });
 
-      console.log(
-        "diablo lp",
+  it("user is not able to cheat pool contract", async () => {
+    await lpToken.connect(diablo).collectRewards();
+
+    expect((await myToken.balanceOf(diablo.address)).toString()).to.be.equal(
+      "99999999999999990"
+    );
+
+    await lpToken.connect(diablo).transfer(cheater.address, parseUnits("2"));
+
+    expect((await lpToken.balanceOf(cheater.address)).toString()).to.be.equal(
+      parseUnits("2")
+    );
+
+    expect((await myToken.balanceOf(cheater.address)).toString()).to.be.equal(
+      "0"
+    );
+
+    await lpToken.connect(cheater).collectRewards();
+
+    expect((await myToken.balanceOf(cheater.address)).toString()).to.be.equal(
+      "0"
+    );
+  });
+  it("function withdraw works correctly", async () => {
+    expect((await lpToken.balanceOf(diablo.address)).toString()).to.be.equal(
+      parseUnits("30")
+    );
+    await lpToken
+      .connect(diablo)
+      .approve(
+        pool.address,
         (await lpToken.balanceOf(diablo.address)).toString()
       );
+    await pool.connect(diablo).withdraw();
+    expect((await lpToken.balanceOf(diablo.address)).toString()).to.be.equal(
+      parseUnits("0")
+    );
 
-      console.log("stake", (await pool.userStake(diablo.address)).toString());
-
-      await myToken.mint(alwin.address, 20000);
-      //   await myToken.mint(poolOwner.address, 20000);
-
-      await myToken.connect(alwin).approve(borrower.address, 2000);
-
-      await myToken.connect(pool).approve(flashLoan.address, 1000000); //   ERR
-
-      await flashLoan
-        .connect(alwin)
-        .borrowTokens(borrower.address, myToken.address, 1000);
-
-      //   console.log(
-      //     "token pool",
-      //     (await myToken.balanceOf(pool.address)).toString()
-      //   );
-      //   console.log(
-      //     "token alwin",
-      //     (await myToken.balanceOf(alwin.address)).toString()
-      //   );
-    });
+    expect((await myToken.balanceOf(diablo.address)).toString()).to.be.equal(
+      "30099999999999999990"
+    );
   });
 });

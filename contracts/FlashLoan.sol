4ff9cd9918ab4be6fa46a2ed9434f707eda19f07
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-11-25 16:21:54 +0100

    call method sendReward from Pool contract

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -7,53 +7,45 @@ import "./interfaces/IBorrowers.sol";
 import "./Pool.sol";
 
 contract FlashLoan is Ownable, IFlashLoan, ReentrancyGuard {
-  uint256 public fee;
-  address public poolAddress;
+    uint256 public fee;
+    address public poolAddress;
 
-  constructor(uint256 _fee, address _poolAddress) {
-    fee = _fee;
-    poolAddress = _poolAddress;
-  }
+    constructor(uint256 _fee, address _poolAddress) {
+        fee = _fee;
+        poolAddress = _poolAddress;
+    }
 
-  function borrowTokens(
-    address _userContract,
-    address _tokenAddress,
-    uint256 _amount
-  ) public override nonReentrant {
-    require(
-      IERC20(_tokenAddress).balanceOf(poolAddress) >= _amount,
-      "Amount exceeds balance"
-    );
-    require(
-      IERC20(_tokenAddress).allowance(poolAddress, address(this)) >= _amount,
-      "Allowance is not set"
-    );
+    function borrowTokens(
+        address _userContract,
+        address _tokenAddress,
+        uint256 _amount
+    ) public override nonReentrant {
+        require(
+            IERC20(_tokenAddress).balanceOf(poolAddress) >= _amount,
+            "Amount exceeds balance"
+        );
+        require(
+            IERC20(_tokenAddress).allowance(poolAddress, address(this)) >=
+                _amount,
+            "Allowance is not set"
+        );
 
-    IERC20(_tokenAddress).transferFrom(poolAddress, msg.sender, _amount);
+        IERC20(_tokenAddress).transferFrom(poolAddress, msg.sender, _amount);
 
-    uint256 beforeTokens = IERC20(_tokenAddress).balanceOf(poolAddress);
+        uint256 repayment = _amount / fee + _amount;
 
-    uint256 repayment = _amount / fee + _amount;
+        require(
+            repayment <= IERC20(_tokenAddress).balanceOf(msg.sender),
+            "You don't have not enough tokens"
+        );
 
-    require(
-      repayment <= IERC20(_tokenAddress).balanceOf(msg.sender),
-      "You don't have not enough tokens"
-    );
+        Borrowers(_userContract).sendBackTokens( //transfer repayment to pool
+            _tokenAddress,
+            poolAddress,
+            repayment
+        );
 
-    Borrowers(_userContract).sendBackTokens(
-      _tokenAddress,
-      poolAddress,
-      repayment
-    );
-
-    uint256 afterTokens = IERC20(_tokenAddress).balanceOf(poolAddress);
-
-    require(
-      beforeTokens + repayment == afterTokens,
-      "Incorrect returned value"
-    );
-
-    uint256 reward = _amount / fee;
-    Pool(poolAddress).distributeReward(reward);
-  }
+        uint256 reward = _amount / fee;
+        Pool(poolAddress).sendReward(reward);
+    }
 }
